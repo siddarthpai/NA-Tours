66fa5b9784d20ca643a14bba4fdab531d69515d5
Siddarth Pai <siddarthdpai@gmail.com>
2023-07-24 12:21:33 +0530

    updating model

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,21 +1,58 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Name must be provided for the tour!'],
-    unique: true
+    unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
-    required: [true, 'Tour must have a price!']
-  }
-})
+    required: [true, 'Tour must have a price!'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'], //works only on strings; removes whitespaces from front and back
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an image'],
+  }, //ref to the image
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  }, // the time when the tour was created.
+  startDates: [Date],
+});
 
-const Tour = mongoose.model('Tour', tourSchema) //first param is name of the model and second is the schema
+const Tour = mongoose.model('Tour', tourSchema); //first param is name of the model and second is the schema
 
-module.exports = Tour
+module.exports = Tour;
